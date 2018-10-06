774c0a9369bc4be2a11b7ddeceb4e531922feb91
zarielan <28706022+zarielan@users.noreply.github.com>
2018-10-06 13:10:09 +0800

    Added win ending

--- Pac-ball v2/Assets/Scripts/GUIHandler.cs
+++ Pac-ball v2/Assets/Scripts/GUIHandler.cs
@@ -12,18 +12,43 @@ public class GUIHandler : MonoBehaviour
     private float totalPickups;
     private float pickedUp;
 
+    public Image overlay;
+    public Text endingText;
+    public Text endingDescription;
+    public CanvasGroup ending;
+
+    private bool playing;
+    private float currentTime;
+
     private void Start()
     {
+        playing = true;
         player = GameObject.Find("Player").GetComponent<Player>();
         totalPickups = GameObject.FindGameObjectsWithTag("Pick Up").Length;
     }
 
     private void Update()
     {
-        while (totalPickups <= 0)
-            totalPickups = GameObject.FindGameObjectsWithTag("Pick Up").Length;
+        if (playing)
+        {
+            while (totalPickups <= 0)
+                totalPickups = GameObject.FindGameObjectsWithTag("Pick Up").Length;
 
-        pickedUp = player.points;
-        ui.text = string.Format("Time: {0:0.00}s{1}Points: {2} / {3} ({4:0.00}%)", Time.time, Environment.NewLine, pickedUp, totalPickups, pickedUp / totalPickups * 100);
+            pickedUp = player.points;
+            currentTime = Time.time;
+
+            ui.text = string.Format("Time: {0:0.00}s{1}Points: {2} / {3} ({4:0.00}%)", currentTime, Environment.NewLine, pickedUp, totalPickups, pickedUp / totalPickups * 100);
+
+            if (pickedUp >= totalPickups)
+                playing = false;
+        }
+        else
+        {
+            endingDescription.text = string.Format("You finished the game in {0:0.00} seconds!", currentTime);
+            endingText.text = "You Won! :D";
+
+            ending.alpha = 1f;
+            ending.blocksRaycasts = true;
+        }
     }
 }
--- Pac-ball v2/Assets/Scripts/Player.cs
+++ Pac-ball v2/Assets/Scripts/Player.cs
@@ -10,21 +10,28 @@ public class Player : MonoBehaviour
     public float speed;
     public int points;
 
+    private int totalPickups;
+
     // Use this for initialization
     void Start ()
     {
         player = GetComponent<Rigidbody>();
         points = 0;
+
+        totalPickups = GameObject.FindGameObjectsWithTag("Pick Up").Length;
     }
 
     private void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
+        if (points < totalPickups)
+        {
+            float moveHorizontal = Input.GetAxisRaw("Horizontal");
+            float moveVertical = Input.GetAxisRaw("Vertical");
 
-        move = new Vector3(moveHorizontal, 0f, moveVertical);
+            move = new Vector3(moveHorizontal, 0f, moveVertical);
 
-        player.AddForce(move * speed);
+            player.AddForce(move * speed);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
