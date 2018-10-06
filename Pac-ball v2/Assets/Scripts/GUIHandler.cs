c13d2aea64be497f077a4eb463fd73f3de97617c
zarielan <28706022+zarielan@users.noreply.github.com>
2018-10-06 11:52:30 +0800

    Added time counter

---
+++ Pac-ball v2/Assets/Scripts/GUIHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIHandler : MonoBehaviour
+{
+    public Text time;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        time.text = string.Format("Time: {0:0.00}s", Time.time);
+    }
+}
